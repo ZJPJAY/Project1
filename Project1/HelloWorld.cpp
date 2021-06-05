cd46e37eab85b867560d5082225313760bf2df56
JB <563902816@qq.com>
2021-06-05 18:28:23 +0800

    add =head vector and print each of vector

--- Project1/HelloWorld.cpp
+++ Project1/HelloWorld.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello Fucking World!" << endl;
+	vector<int> vec = { 1,2,3,4,5,6,7,8,9,0 };
 
+	cout << "Hello Fucking World!" << endl;
 	cout << "Change Something" << endl;
+	for (int i = 0; i < 10; i++) {
+		cout << vec[i] << " ";
+	}
 
 	return 0;
 }
